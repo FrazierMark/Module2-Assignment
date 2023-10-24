3d7d2567d9ea2a6f6a6c75603ac6ff44f5859a93
FrazierMark <m.mark.frazier@gmail.com>
2023-10-24 11:55:07 -0700

    Completed favorite things route

--- app.py
+++ app.py
@@ -17,22 +17,49 @@ def homepage():
 @app.route('/froyo')
 def choose_froyo():
     """Shows a form to collect the user's Fro-Yo order."""
-    pass
+    return """
+    <form action="/froyo_results" method="GET">
+        What is your favorite Fro-Yo flavor? <br/>
+        <input type="text" name="flavor"><br/>
+        What toppings would you like on your Fro-Yo? <br/>
+        <input type="text" name="toppings"><br/>
+        <input type="submit" value="Submit!">
+    </form>
+    """
 
 @app.route('/froyo_results')
 def show_froyo_results():
     """Shows the user what they ordered from the previous page."""
-    pass
+    users_froyo_flavor = request.args.get('flavor')
+    users_froyo_toppings = request.args.get('toppings')
+    return f'You ordered {users_froyo_flavor} flavored Fro-Yo with the following toppings: {users_froyo_toppings}!'
 
 @app.route('/favorites')
 def favorites():
     """Shows the user a form to choose their favorite color, animal, and city."""
-    pass
+    return """
+    <form action="/favorites_results" method="GET">
+        What is your favorite color? <br/>
+        <input type="text" name="color"><br/>
+        
+        What is your favorite animal? <br/>
+        <input type="text" name="animal"><br/>
+        
+        What is your favorite city? <br/>
+        <input type="text" name="city"><br/>
+        
+        <input type="submit" value="Submit!">
+    </form>
+    """
+
 
 @app.route('/favorites_results')
 def favorites_results():
     """Shows the user a nice message using their form results."""
-    pass
+    users_favorite_color = request.args.get('color')
+    users_favorite_animal = request.args.get('animal')
+    users_favorite_city = request.args.get('city')
+    return f"Wow, I didn't know {users_favorite_color} {users_favorite_animal} lived in {users_favorite_city}!"
 
 @app.route('/secret_message')
 def secret_message():
